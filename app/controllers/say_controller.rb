c7020aff0352d05362675a146211ccf5b47cbb9e
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-10-28 22:50:32 -0700

    Getting files from a folder and displaying them

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -4,5 +4,14 @@ class SayController < ApplicationController
   end
 
   def goodbye
+    @files = Dir.glob("*")
+    username = "rohitvemuri"
+    @folder_name = "Desktop/PokemonEmeraldWalkthroughs"
+    directory_path = File.expand_path("~#{username}/#{@folder_name}")
+    if File.directory?(directory_path)
+      @files1 = Dir.entries(directory_path).reject { |file| file == "." || file == ".." }
+    else
+      @error_message = "Directory not found: #{directory_path}"
+    end
   end
 end
